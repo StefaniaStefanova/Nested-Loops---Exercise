b991c67928dde561613aab7ca7e466f874c943a2
StefaniaStefanova <StefaniaStefanova@users.noreply.github.com>
2023-12-22 23:54:52 +0200

    Add project files.

---
+++ 05. Special Numbers/Program.cs
@@ -0,0 +1,33 @@
+﻿namespace _05._Special_Numbers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            for (int i = 1111; i <= 9999; i++)
+            {
+                int number = i;
+                bool isSpecial = true;
+
+                while (number > 0)
+                {
+                    int digit = number % 10;
+                    number /= 10;
+
+                    if (digit == 0 || n % digit != 0)
+                    {
+                        isSpecial = false;
+                        break;
+                    }
+                }
+
+                if (isSpecial)
+                {
+                    Console.Write(i + " ");
+                }
+            }
+        }
+    }
+}
